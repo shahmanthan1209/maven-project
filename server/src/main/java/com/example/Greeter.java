3c1c9ced2368fb8699a2b013d3ad42bdb9ab0996
shahmanthan1209 <shahmanthan1209@gmail.com>
2018-01-21 11:01:01 +0530

    webhook

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,6 +13,6 @@ public class Greeter {
   }
 
   public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello,%s!", someone);
   }
 }
